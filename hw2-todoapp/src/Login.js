f24b6d6f4d037e209d8a6a247e7b77c16cd35b97
MrRogerss <naterogers29@yahoo.com>
2022-05-29 18:07:06 -0500

    hw4

--- hw2-todoapp/src/Login.js
+++ hw2-todoapp/src/Login.js
@@ -1,5 +1,5 @@
 
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import { useResource } from "react-request-hook";
 
 export default function Login({dispatch}) {
@@ -14,10 +14,21 @@ export default function Login({dispatch}) {
     url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
     method: 'get'
     }))
+
+    useEffect(() => {
+      if (user && user.data) {
+      if (user.data.length > 0) {
+      setLoginFailed(false)
+      dispatch({ type: 'LOGIN', username: user.data[0].username })
+      } else {
+      setLoginFailed(true)
+      }
+      }
+      }, [user])
     
 
   return (
-    <form onSubmit={(e) => {e.preventDefault(); dispatch({type: "LOGIN",username})}}>
+    <form onSubmit={e => { e.preventDefault(); login(username, password) }}>
       <label htmlFor="login-username">Username:</label>
       <input type="text" name="login-username" value={username} onChange={handleUsername}
  id="login-username" />
@@ -25,6 +36,8 @@ export default function Login({dispatch}) {
       <input type="password" value={password} onChange={handlePassword} name="login-username" id="login-username" />
       
       <input type="submit" value="Login" disabled={username.length === 0} />
+      
+      {loginFailed && <span style={{ color: 'red' }}>Invalid username or password</span>}
     </form>
   );
 }
